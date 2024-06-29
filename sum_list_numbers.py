350dab3e15782befbb2326b93c84c63c2784e514
= <=>
2024-06-29 01:22:36 +0000

    sum of numbers in a list

---
+++ sum_list_numbers.py
@@ -0,0 +1,7 @@
+# Write a Python program using a for loop to calculate the sum of all the numbers in a list.
+numbers = [1, 5, 3, 9]
+
+total = 0;
+for num in numbers:
+    total += num
+print("The sum of the numbers in the list is: ",total)
