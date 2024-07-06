45c4eb4228c906961730cb60289e6fa9acf8059d
= <=>
2024-07-06 05:37:48 +0000

    modify weight_convertor.py

--- weight_convertor.py
+++ weight_convertor.py
@@ -4,21 +4,23 @@ unit = input("L (lbs) or K (kgs): ").lower()
 
 # convert the weight from one unit to another based on the unit input 
 # using match cases 
-match unit:
-    case "l":
-        converted = weight * 0.45
-        print(f"You are {round(converted, 2)} kilos")
-    case "k":
-        converted = weight / 0.45
-        print(f"You are {round(converted, 2)} pounds")
-    case _:
-        print("Enter a valid weight and unit")
+# match unit:
+#     case "l":
+#         converted = weight * 0.45
+#         print(f"You are {round(converted, 2)} kilos")
+#     case "k":
+#         converted = weight / 0.45
+#         print(f"You are {round(converted, 2)} pounds")
+#     case _:
+#         print("Enter a valid weight and unit")
 
 # using if statememts 
-# if unit == "l":
-#     converted = weight * 0.45
-#     print(f"You are {converted} kilos")
-# elif unit == "k":
-#     converted = weight / 0.45
-#     print(f"You are {converted} pounds")
+if unit == "l":
+    converted = weight * 0.45
+    print(f"You are {round(converted, 2)} kilos")
+elif unit == "k":
+    converted = weight / 0.45
+    print(f"You are {round(converted, 2)} pounds")
+else:
+    print("Enter a valid weight and unit")
 
