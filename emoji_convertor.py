c0610b800db0bd8cff863ce4e9abf795bfa92a70
= <=>
2024-07-07 19:53:49 +0000

    modify emoji_convertor.py

--- emoji_convertor.py
+++ emoji_convertor.py
@@ -1,13 +1,18 @@
 message = input("> ")
-words = message.split(" ")
 
-emoji_mapping = {
+def emoji_convertor(message):
+    words = message.split(" ")
+
+    emoji_mapping = {
     ":)":"😊",
     ":(":"😢"
-}
+    }
 
-output =""
-for word in words:
-    output += emoji_mapping.get(word, word) + " "
-print(output)
+    output =""
+    for word in words:
+        output += emoji_mapping.get(word, word) + " "
+    return output
+
+    
+print(emoji_convertor(message))
 
