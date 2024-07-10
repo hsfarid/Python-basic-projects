4ef11bb8f3d3f14bc173c267e4bc708c2a03c971
Farid Habib <hsfarid08@gmail.com>
2024-07-10 11:11:40 +0000

    add guess_number.py

---
+++ guess_number.py
@@ -0,0 +1,39 @@
+import random as rd
+
+#generate a secret number between 1 and 10
+secret_number = range(rd.randint(1, 10))
+
+
+#prompt user to guess the number
+guess = int(input("Guess the number: "))
+
+match guess:
+    case 1:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 2:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 3:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 4:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 5:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 6:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 7:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 8:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+    case 9:
+        if guess == secret_number:
+            print("congratulations, you guessed it!")
+
+   
